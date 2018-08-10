a35075791f92bd4a234d12c6cb8376de43d3f800
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 13:31:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     nil
   else
-    # minVal = name_hash.values[0]
-    minVal = 0
-    # keyMinVal = name_hash.keys[0]
-    keyMinVal = ""
+    minVal = name_hash.values[0]
+    # minVal = 0
+    keyMinVal = name_hash.keys[0]
+    # keyMinVal = ""
+    binding.pry
     name_hash.each{|key, value|
       if value < minVal
         minVal = value
